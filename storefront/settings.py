216c921af7418066f991dca357eb6d715e6bd053
acu8 <ewatanabe786@gmail.com>
2023-06-01 22:49:31 +0900

    django debug toolbar

--- storefront/settings.py
+++ storefront/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'playground',
     'store',
     'tags',
-    'likes'
+    'likes',
+    "django.contrib.staticfiles",
+    'debug_toolbar', 
 ]
 
 MIDDLEWARE = [
@@ -51,16 +53,30 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     "debug_toolbar.middleware.DebugToolbarMiddleware",
    
 ]
 
+INTERNAL_IPS = [
+
+    "127.0.0.1",
+
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : lambda request: True,
+}
 
 ROOT_URLCONF = 'storefront.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            }
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,7 +85,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            
+
         },
+         
     },
 ]
 
